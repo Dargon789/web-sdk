ef585adbb14d4ffa89a7feed5ed181595f865e0c
Dargon789 <64915515+Dargon789@users.noreply.github.com>
2025-11-25 16:35:47 +0700

    Update WalletListItem.tsx
    Signed-off-by: Dargon789 <64915515+Dargon789@users.noreply.github.com>

--- examples/components/src/WalletListItem.tsx
+++ examples/components/src/WalletListItem.tsx
@@ -1,39 +1,48 @@
-import { truncateAtMiddle } from '@0xsequence/connect'
-import { Button, Card, cn, Text } from '@0xsequence/design-system'
 
-interface WalletListItemProps {
-  id: string
-  name: string
-  address: string
-  isActive: boolean
-  isEmbedded?: boolean
-  onSelect: () => void
-  onDisconnect: () => void
-}
+  import { SequenceConnect, createConfig } from '@0xsequence/connect'
 
-export const WalletListItem = ({ id, name, address, isActive, isEmbedded, onSelect, onDisconnect }: WalletListItemProps) => {
-  return (
-    <Card
-      className={cn(
-        'flex flex-row items-center justify-between relative',
-        isActive ? 'bg-background-secondary' : 'bg-background-muted'
-      )}
-      key={id}
-    >
-      <div className="absolute top-0 left-0 right-0 bottom-0" onClick={onSelect} style={{ cursor: 'pointer', zIndex: 1 }} />
-      <div className="flex flex-row items-center">
-        <div className="border" />
-        <div className="flex flex-col gap-1">
-          <Text variant="normal" color="primary">
-            {isEmbedded ? 'Embedded - ' : ''}
-            {name}
-          </Text>
-          <Text variant="normal" fontWeight="bold" color="primary">
-            {truncateAtMiddle(address, 10)}
-          </Text>
-        </div>
-      </div>
-      <Button variant="text" size="sm" label="Disconnect" onClick={onDisconnect} style={{ position: 'relative', zIndex: 2 }} />
-    </Card>
-  )
+  export function App() {
+    const projectAccessKey = "AQAAAAAAAJbd_5JOcE50AqglZCtvu51YlGI"
+    const waasConfigKey = "eyJwcm9qZWN0SWQiOjM4NjIxLCJycGNTZXJ2ZXIiOiJodHRwczovL3dhYXMuc2VxdWVuY2UuYXBwIn0=" // Pass in your waasConfigKey
+    const enableConfirmationModal = true // change to your preference
+    const googleClientId = 'YOUR_GOOGLE_CLIENT_ID' // Google Client ID
+    const appleClientId = 'YOUR_APPLE_CLIENT_ID' // Apple Client ID
+    const appleRedirectURI = 'YOUR_APP_SCHEME://auth' // Apple Redirect URI
+    const walletConnectProjectId = 'walletConnectProjectId' // Pass in your WalletConnect Project ID
+
+    const config = createConfig('waas', {
+      projectAccessKey,
+      position: "center",
+      defaultTheme: "dark",
+      signIn: {
+        projectName: "web3-game",
+      },
+      defaultChainId: 421614,
+      chainIds: [1, 10, 40, 41, 56, 97, 100, 137, 143, 1101, 1284, 1287, 1868, 1946, 1993, 5031, 6252, 8333, 8453, 10143, 19011, 28802, 33111, 33139, 40875, 42161, 42170, 42793, 43113, 43114, 50312, 80002, 81457, 84532, 128123, 421614, 660279, 747474, 5042002, 11155111, 11155420, 21000000, 37084624, 168587773, 1482601649, 37714555429],
+      appName: "web3-game",
+      email: true,
+      waasConfigKey,
+      google: {
+        clientId: googleClientId
+      },
+      apple: {
+        clientId: appleClientId,
+        redirectURI: appleRedirectURI
+      },
+      walletConnect: {
+        projectId: walletConnectProjectId
+      },
+      coinbase: true,
+      metaMask: true,
+      wagmiConfig: {
+        multiInjectedProviderDiscovery: true,
+      },
+      enableConfirmationModal
+    })
+
+    return (
+      <SequenceConnect config={config}>
+        <MyPage />
+      </SequenceConnect>
+    );
 }
