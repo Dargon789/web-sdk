d5d10fb9d39643200080d25bdbb34142f1706906
Dargon789 <64915515+Dargon789@users.noreply.github.com>
2026-01-01 08:28:37 +0700

    Add App function with configuration settings
    https://sequence.build/project/16538/embedded-wallet
    
    Signed-off-by: Dargon789 <64915515+Dargon789@users.noreply.github.com>

--- examples/next/src/config.ts
+++ examples/next/src/config.ts
@@ -3,6 +3,45 @@ import { ChainId } from '@0xsequence/network'
 import { zeroAddress } from 'viem'
 import { cookieStorage, createStorage } from 'wagmi'
 
+  export function App() {
+    const projectAccessKey = "AQAAAAAAAJbd_5JOcE50AqglZCtvu51YlGI"
+    const waasConfigKey = "eyJwcm9qZWN0SWQiOjM4NjIxLCJycGNTZXJ2ZXIiOiJodHRwczovL3dhYXMuc2VxdWVuY2UuYXBwIn0=" // Pass in your waasConfigKey
+    const enableConfirmationModal = true // change to your preference
+    const googleClientId = 'YOUR_GOOGLE_CLIENT_ID' // Google Client ID
+    const appleClientId = 'YOUR_APPLE_CLIENT_ID' // Apple Client ID
+    const appleRedirectURI = 'YOUR_APP_SCHEME://auth' // Apple Redirect URI
+    const walletConnectProjectId = 'walletConnectProjectId' // Pass in your WalletConnect Project ID
+
+    const config = createConfig('waas', {
+      projectAccessKey,
+      position: "center",
+      defaultTheme: "dark",
+      signIn: {
+        projectName: "sequence.app",
+      },
+      defaultChainId: 421614,
+      chainIds: [1, 10, 40, 41, 56, 97, 100, 137, 143, 1101, 1284, 1287, 1868, 1946, 1993, 5031, 6252, 8333, 8453, 10143, 19011, 24101, 28802, 33111, 33139, 40875, 42161, 42170, 42793, 43113, 43114, 50312, 80002, 81457, 84532, 127823, 128123, 421614, 660279, 747474, 5042002, 11155111, 11155420, 21000000, 37084624, 168587773, 1482601649, 37714555429],
+      appName: "sequence.app",
+      email: true,
+      waasConfigKey,
+      google: {
+        clientId: googleClientId
+      },
+      apple: {
+        clientId: appleClientId,
+        redirectURI: appleRedirectURI
+      },
+      walletConnect: {
+        projectId: walletConnectProjectId
+      },
+      coinbase: true,
+      metaMask: true,
+      wagmiConfig: {
+        multiInjectedProviderDiscovery: true,
+      },
+      enableConfirmationModal
+    })
+
 export const isDebugMode = false
 
 const projectAccessKey = 'AQAAAAAAAEGvyZiWA9FMslYeG_yayXaHnSI'
@@ -87,4 +126,9 @@ export const config = createConfig('waas', {
     storage: createStorage({ storage: cookieStorage }),
     ssr: true
   }
-})
+   return (
+      <SequenceConnect config={config}>
+        <MyPage />
+      </SequenceConnect>
+    );
+}
